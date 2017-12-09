535f422fa2e60587ab1b2b8eb59d1c4ddbf29e79
qq215236213 <215236213@qq.com>
2017-12-09 22:27:23 +0800

    test

---
+++ WebApplication1/Models/QueueHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+	public class QueueHelper
+	{
+		public readonly static QueueHelper Instance = new QueueHelper();
+
+		private QueueHelper() { }
+		private Queue<string> msgList = new Queue<string>();
+		private WebSocket socket = null;
+
+		public void SetSocket(WebSocket socket)
+		{
+			this.socket = socket;
+		}
+		public void SetMsg(string msg)
+		{
+			msgList.Enqueue(msg);
+		}
+
+		public void Start()
+		{
+			Thread thread = new Thread(ThreadStart);
+			thread.IsBackground = true;
+			thread.Start();
+		}
+
+		private void ThreadStart()
+		{
+			while (true)
+			{
+				if(msgList.Count > 0)
+				{
+					try
+					{
+						ScanQueue();
+					}
+					catch(Exception ex)
+					{
+						throw new Exception(ex.Message);
+					}
+				}
+				else
+				{
+					Thread.Sleep(3000);
+				}
+			}
+		}
+
+		private void ScanQueue()
+		{
+			while(msgList.Count > 0)
+			{
+				try
+				{
+					//获取队列中的消息
+					string msg = msgList.Dequeue();
+					var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+					this.socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+				}
+				catch(Exception ex)
+				{
+					throw new Exception(ex.Message);
+				}
+			}
+		}
+	}
+}
