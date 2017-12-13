5e4e8351d1a767da7a7fb336eb1f66859407aa29
qq215236213 <215236213@qq.com>
2017-12-13 22:39:43 +0800

    test

--- Service/OtherService/MyService.cs
+++ Service/OtherService/MyService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -12,12 +13,37 @@ namespace OtherService
 	{
 		[OperationContract]
 		int AddInt(int a, int b);
+
+		[OperationContract]
+		bool UpLoadFile(Stream streamInput);
 	}
 	public class MyService : IService
 	{
 		public int AddInt(int a, int b)
 		{
 			return a + b;
+		}
+
+		public bool UpLoadFile(Stream streamInput)
+		{
+			bool isSuccess = false;
+			try
+			{
+				using(var outputStream = new FileStream("", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					streamInput.CopyTo(outputStream);
+					outputStream.Flush();
+					isSuccess = true;
+					Console.WriteLine("在{0}接收到客户端发送的流，已保存到rec.map3。", DateTime.Now.ToLongTimeString());
+				}
+			}
+			catch(Exception ex)
+			{
+				isSuccess = false;
+			}
+
+
+			return isSuccess;
 		}
 	}
 }
