160d85abc97bd5a15f8f0c047c74c048d60967df
qq215236213 <215236213@qq.com>
2017-12-18 19:53:10 +0800

    12

--- Service/WindowsFormClient/MessageContent.cs
+++ Service/WindowsFormClient/MessageContent.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WindowsFormClient
 {
-    public class MessageContent
-    {
-        public string NickName { get; set; }
-        public string Message { get; set; }
-        public DateTime Time { get; set; }
-        public bool IsMe { get; set; }
-    }
+	public class MessageContent
+	{
+		public string NickName { get; set; }
+		public string Message { get; set; }
+		public DateTime Time { get; set; }
+		public bool IsMe { get; set; }
+	}
 }
